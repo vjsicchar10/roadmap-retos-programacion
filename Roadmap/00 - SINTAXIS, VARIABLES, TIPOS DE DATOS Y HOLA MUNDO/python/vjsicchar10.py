8dff767b4ccf87b2b54c4dacd3cc0b380a47be3e
vjsicchar10 <vjsicchar@gmail.com>
2026-01-12 14:41:28 -0500

    Update vjsicchar10.py

--- Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/vjsicchar10.py
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/vjsicchar10.py
@@ -1,1 +1,15 @@
+# https://www.python.org/
 
+# Sintaxis de una sola línea
+"Sintaxis de una sola línea"
+
+# Sintaxis de varias líneas
+"""
+Puedo realizar diferntes comentarios
+en diversas líneas de código
+"""
+'''
+De la siguiente manera
+Tambien se puede realizar
+Comentarios largos
+'''
